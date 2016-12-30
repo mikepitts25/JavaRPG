ba5afd3cedc535f62c877f13eab71b77fa92f3a9
Mike Pitts <mikepitts25@gmail.com>
2016-12-30 17:50:10 -0500

    Continued building out the Player class.

--- src/JavaRPG.java
+++ src/JavaRPG.java
@@ -5,31 +5,15 @@
  * Purpose: Just finished Intro to Programming (Java) and want to see what kind of game I can make =)
  * I will add to and modify this code as I learn more about the language.
  */
-import java.util.Scanner;
+import java.io.IOException;
 
 public class JavaRPG {
 
-  public static void main(String[] args) {
-    String firstName;
-    String lastName;
-    int gender;
+  public static void main(String[] args) throws IOException {
+    Player myPlayer = new Player();
 
-    Scanner nameGrabber = new Scanner(System.in);
+    myPlayer.playerCreate();
+    myPlayer.getStats();
 
-    //set player fields to send to constructor
-    System.out.print("By what name do you wish to be known? ");
-    firstName = nameGrabber.next();
-    System.out.println("Pleasure to meet you " + firstName + ".");
-    System.out.print("What is your surname? ");
-    lastName = nameGrabber.next();
-    System.out.print("Lastly, what is your gender? ");
-    gender = nameGrabber.nextInt();
-
-    nameGrabber.close();
-
-    Player myPlayer = new Player(firstName, lastName, gender);
-    System.out.println("Excellent. I have you registered as " + myPlayer.getFirstName() + " " +
-        myPlayer.getLastName() + ".");
-    System.out.println(myPlayer.getGender() + " will be your documented gender.");
   }
 }
--- src/Player.java
+++ src/Player.java
@@ -6,15 +6,17 @@
  */
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Player {
   // player fields
   private String firstName;
   private String lastName;
-  private int gender = 1;
+  private String className;
+  private String gender;
   private int level = 1;
   private int expTotal = 0;
-  private int strengthLevel = 10;
+  private int strengthLevel;
   private int energyLevel = 100;
   private int magicForce = 100;
   private int lifeForce = 100;
@@ -22,17 +24,21 @@ public class Player {
   public Player() { //default constructor
   }
 
-  public Player(String firstName, String lastName, int gender) { //constructor with name fields
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.gender = gender;
+  //Getters and Setters
+
+  public String getClassName() {
+    return className;
   }
 
-  public int getGender() {
+  public void setClassName(String className) {
+    this.className = className;
+  }
+
+  public String getGender() {
     return gender;
   }
 
-  public void setGender(int gender) {
+  public void setGender(String gender) {
     this.gender = gender;
   }
 
@@ -101,18 +107,175 @@ public class Player {
   }
 
   public void chooseClass(){
-    int classChoice = 0;
+    int classChoice;
+    boolean choose = true;
     Scanner stdin = new Scanner(System.in);
-    System.out.print("Choose your class: \n" +
-        "(1) Warrior \n" +
-        "(2) Rogue \n" +
-        "(3) Wizard \n" +
-        "(4) Archer ");
 
-    classChoice = stdin.nextInt();
-    stdin.close();
+
+    //loop through the class selection until we get a valid input from the user
+    do {
+
+      System.out.print("Choose your class: \n" +
+          "(1) Warrior \n" +
+          "(2) Rogue \n" +
+          "(3) Wizard \n" +
+          "(4) Ranger ");
+
+      //handle some Exceptions caused by input mismatch, etc.
+      try {
+
+        classChoice = stdin.nextInt();
+
+        switch (classChoice){
+          case 1:
+            setClassName("Warrior");
+            choose = false;
+            break;
+          case 2:
+            setClassName("Rogue");
+            choose = false;
+            break;
+          case 3:
+            setClassName("Wizard");
+            choose = false;
+            break;
+          case 4:
+            setClassName("Ranger");
+            choose = false;
+            break;
+          default:
+            System.out.println("Invalid selection. Please try again.");
+            break;
+        }
+
+      } catch (Exception e) {
+
+        System.out.println("You can't do that!");
+        stdin.next(); //need this to consume last scanner input and ask for another
+
+      }
+
+    } while (choose);
+
+    System.out.printf("You have chosen the way of the %s. Good luck.\n", getClassName());
 
   }
 
+  public void chooseGender(){
+    int genderChoice;
+    boolean choose = true;
+    Scanner stdin = new Scanner(System.in);
+
+
+    //loop through the gender selection until we get a valid input from the user
+    do {
+
+      System.out.print("Are you male or female?\n" +
+          " (1) Male\n" +
+          " (2) Female ");
+
+      //handle some Exceptions caused by input mismatch, etc.
+      try {
+
+        genderChoice = stdin.nextInt();
+
+        switch (genderChoice){
+          case 1:
+            setGender("male");
+            choose = false;
+            break;
+          case 2:
+            setGender("female");
+            choose = false;
+            break;
+          default:
+            System.out.println("Invalid selection. Please try again.");
+            break;
+        }
+
+      } catch (Exception e) {
+
+        System.out.println("You can't do that!");
+        stdin.next(); //need this to consume last scanner input and ask for another
+
+      }
+
+    } while (choose);
+    System.out.printf("You are a %s. \n", getGender());
+
+  }
+
+  public void strLvl(){
+    boolean again = true;
+    Random rand = new Random();
+    int strLvl;
+    int choice;
+    Scanner stdin = new Scanner(System.in);
+
+    do {
+
+      strLvl = 1 + rand.nextInt(15);
+      System.out.println("Strength is: "+ strLvl + ".");
+      System.out.print("Reroll strength? \n" +
+          "(1) Yes \n" +
+          "(2) No ");
+
+      try {
+        choice = stdin.nextInt();
+
+        switch (choice) {
+          case 1:
+            System.out.println("Rerolling...");
+            break;
+          case 2:
+            setStrengthLevel(strLvl);
+            again = false;
+            break;
+          default:
+            System.out.print("Invalid response. Try again.");
+            break;
+        }
+      } catch (Exception e) {
+        System.out.println("You can't do that!");
+        stdin.next();
+      }
+
+    } while (again);
+
+    System.out.printf("Your strength is set to %d.\n", getStrengthLevel());
+
+  }
+
+  public void playerCreate(){
+    String fName;
+    String lName;
+    Scanner stdin = new Scanner(System.in);
+
+    System.out.print("Please enter your character's first name: ");
+    fName = stdin.next();
+    setFirstName(fName);
+
+    System.out.print("Please enter your character's last name: ");
+    lName = stdin.next();
+    setLastName(lName);
+
+    System.out.printf("Welcome to this super fun JavaRPG game, %s %s.\n", getFirstName(), getLastName());
+
+    chooseClass();
+    chooseGender();
+    strLvl();
+
+  }
+
+  public void getStats(){
+    System.out.printf("Character Name: %s %s\n" +
+        "Level: %d\n" +
+        "Experience: %d\n" +
+        "Strength: %d\n" +
+        "Life Force: %d\n" +
+        "Magic Force: %d \n" +
+        "Energy Level: %d", getFirstName(), getLastName(), getLevel(), getExpTotal(), getStrengthLevel(), getLifeForce(),
+        getMagicForce(), getEnergyLevel());
+  }
 
 }
